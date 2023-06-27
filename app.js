9ebc77dc2767389008cdb6cdfb219384f3564822
unknown <abumohammadnayemkhan@gmail.com>
2023-06-28 00:01:42 +0600

    learning

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict"; //strict oparatore are resived sili mitechk, that misteck are not recognaized by jsvascript engin
+
+function myName(){
+    myName = "hasan";
+}
+
+myName();
---
+++ sprad.js
@@ -0,0 +1,7 @@
+//sprade oporator attach array with each other.
+
+let poorCountry = ["Bangladesh", "Srilanka"];
+let richCountry = [...poorCountry,"America", "Canada", "Italy"];
+
+console.log(richCountry);
+
